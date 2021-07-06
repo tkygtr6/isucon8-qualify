628ee8bad08ef479f481a506595bf8cfdfb26874
tkygtr6 <tkygtr6@gmail.com>
2021-07-06 22:18:41 +0900

    Fix

--- scripts/deploy_from_local.sh
+++ scripts/deploy_from_local.sh
@@ -12,7 +12,7 @@ echo "Deploying isucon-server2"
 ssh $SERVER2_HOSTNAME "cd /home/isucon/torb && git pull"
 ssh $SERVER2_HOSTNAME "sudo /home/isucon/torb/scripts/deploy.sh isucon-server2"
 
-echo "Deploying isucon-server3"
+echo "Deploying isucon-server1"
 ssh $SERVER1_HOSTNAME "cd /home/isucon/torb && git pull"
-ssh $SERVER1_HOSTNAME "sudo /home/isucon/torb/scripts/deploy.sh isucon-server3"
+ssh $SERVER1_HOSTNAME "sudo /home/isucon/torb/scripts/deploy.sh isucon-server1"
 
